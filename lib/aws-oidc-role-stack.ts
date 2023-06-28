2453d800bc7f60c7f1c72617a43c594b229fbaa5
Kenji Saito <ken-yo@mbr.nifty.com>
2023-06-28 10:57:59 +0900

    Add the thumprint for GitHub OIDC Provider

--- lib/aws-oidc-role-stack.ts
+++ lib/aws-oidc-role-stack.ts
@@ -20,7 +20,8 @@ export class AwsOidcRoleStack extends cdk.Stack {
         'sts.amazonaws.com'
       ],
       thumbprintList: [
-        '6938fd4d98bab03faadb97b34396831e3780aea1'
+        '6938fd4d98bab03faadb97b34396831e3780aea1',
+        '1c58a3a8518e8759bf075b76b750d4f2df264fcd'
       ]
     });
 
