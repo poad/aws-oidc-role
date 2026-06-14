d57968cbed46732022c4722e8cee5ba08d428cf9
Kenji Saito <ken-yo@mbr.nifty.com>
2026-06-15 08:35:19 +0900

    Bumps node modules

--- update.sh
+++ update.sh
@@ -21,11 +21,10 @@ if [ $result -ne 0 ]; then
 fi
 echo ""
 pwd
-npx -y pnpm@latest self-update && pnpm install && pnpm up -r && pnpm audit --fix override && pnpm up -r && pnpm lint-fix && pnpm build && pnpm install --no-frozen-lockfile
-result=$?
-if [ $result -ne 0 ]; then
+
+if ! (pnx -y pnpm@latest self-update && pnpm install && pnpm up -r && pnpm audit --fix override && pnpm up -r && pnpm lint-fix && pnpm build && pnpm install --no-frozen-lockfile); then
   cd "${CUR}" || exit
-  exit $result
+  exit 1
 fi
 
 cd "${CURRENT}" || exit
@@ -34,9 +33,8 @@ if [ $result -ne 0 ]; then
   cd "${CUR}" || exit
   exit $result
 fi
-git pull --prune && git commit -am "Bumps node modules" && git push
-result=$?
-if [ $result -ne 0 ]; then
+
+if ! (git pull --prune && git commit -am "Bumps node modules" && git push); then
   cd "${CUR}" || exit
   exit $result
 fi
